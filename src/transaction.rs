f70dd02bc000653348327c4696b9edacb61fb584
gthecht <gdhecht@gmail.com>
2024-11-24 17:32:33 +0200

    Use simple date

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,17 +1,93 @@
 use color_eyre::Result;
+use core::fmt;
 use ratatui::{
     text::Text,
     widgets::{Cell, Row},
 };
 use serde::{Deserialize, Serialize};
-use std::{cmp::Ordering, str::FromStr};
-use time::{format_description, macros::format_description, OffsetDateTime};
+use std::{cmp::Ordering, fmt::Display, str::FromStr};
+use time::{Date, Month};
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct SimpleDate {
+    pub year: i32,
+    pub month: u8,
+    pub day: u8,
+    date: Date,
+}
+
+impl SimpleDate {
+    pub fn from_string(date_string: &str) -> Result<SimpleDate, &str> {
+        let splitter = if date_string.contains("-") { "-" } else { "." };
+        let mut split_str = date_string.split(splitter);
+        if let Some(year) = split_str.next() {
+            if let Ok(year) = i32::from_str(year) {
+                let year = if year < 100 { 2000 + year } else { year };
+                if let Some(month) = split_str.next() {
+                    if let Ok(month) = u8::from_str(month) {
+                        if let Ok(m) = Month::try_from(month) {
+                            if let Some(day) = split_str.next() {
+                                if let Ok(day) = u8::from_str(day) {
+                                    if let Ok(date) = Date::from_calendar_date(year, m, day) {
+                                        return Ok(SimpleDate {
+                                            year,
+                                            month,
+                                            day,
+                                            date,
+                                        });
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Err("failed to create date from string")
+    }
+}
+
+impl Display for SimpleDate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:04}.{:02}.{:02}", self.year, self.month, self.day)
+    }
+}
+
+impl Serialize for SimpleDate {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&format!("{}", self))
+    }
+}
+
+impl<'de> Deserialize<'de> for SimpleDate {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        SimpleDate::from_string(&s).map_err(serde::de::Error::custom)
+    }
+}
+
+impl PartialOrd for SimpleDate {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.date.cmp(&other.date))
+    }
+}
+
+impl Ord for SimpleDate {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.date.cmp(&other.date)
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Transaction {
-    #[serde(with = "time::serde::iso8601")]
-    pub date: OffsetDateTime,
+    pub date: SimpleDate,
     amount: f64,
     currency: String,
     details: String,
@@ -38,7 +114,7 @@ impl Column {
 }
 
 impl Transaction {
-    pub fn new(date: OffsetDateTime) -> Self {
+    pub fn new(date: SimpleDate) -> Self {
         Transaction {
             date,
             amount: 0.0,
@@ -61,20 +137,9 @@ impl Transaction {
         }
     }
 
-    fn try_parse_date(&self, input: &str) -> Result<OffsetDateTime> {
-        let format = format_description!(
-            "[year]-[month]-[day] [hour]:[minute]:[second] [offset_hour \
-                 sign:mandatory]"
-        );
-        Ok(OffsetDateTime::parse(
-            &format!("{} 00:08:00 +02", input),
-            &format,
-        )?)
-    }
-
     pub fn mutate_field(&mut self, column: &Column, input: &str) -> Result<(), &str> {
         match column.name() {
-            "Date" => match self.try_parse_date(input) {
+            "Date" => match SimpleDate::from_string(input) {
                 Ok(date) => self.date = date,
                 Err(_) => return Err(" failed to parse as date"),
             },
@@ -93,9 +158,7 @@ impl Transaction {
 
     pub fn generate_row_text(&self) -> [String; 6] {
         [
-            self.date
-                .format(&format_description::parse("[year]-[month]-[day]").unwrap())
-                .unwrap(),
+            format!("{}", self.date),
             format!("{}", self.amount),
             self.details.clone(),
             self.category.clone(),
