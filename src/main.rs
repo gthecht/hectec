fdc5a85d2d356fd1cbb2b40c3b342056574586d1
Gilad Hecht <gdhecht@gmail.com>
2025-01-03 17:28:51 +0200

    Split input_page from App
    Move transaction table and table view into input_page

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
+mod input_page;
 mod logger;
 mod transaction;
 use std::env;
 use std::path::PathBuf;
 
+use crate::input_page::InputPage;
 use crate::logger::initialize_logging;
-use crate::transaction::{TransactionField, TransactionsTable};
+use crate::transaction::TransactionsTable;
 use color_eyre::Result;
 use crossterm::event::{KeyEvent, KeyModifiers};
 use ratatui::{
     crossterm::event::{self, Event, KeyCode, KeyEventKind},
-    layout::{Constraint, Layout, Margin, Position, Rect},
-    style::{self, Color, Modifier, Style, Stylize},
-    text::{Line, Span, Text},
-    widgets::{
-        Block, BorderType, Cell, HighlightSpacing, Paragraph, Row, Scrollbar, ScrollbarOrientation,
-        ScrollbarState, Table, TableState,
-    },
+    layout::{Constraint, Layout, Margin, Rect},
+    style::{self, Color, Style},
+    text::Text,
+    widgets::{Block, BorderType, Paragraph, Scrollbar, ScrollbarOrientation, ScrollbarState},
     DefaultTerminal, Frame,
 };
 use style::palette::tailwind;
@@ -76,122 +75,25 @@ impl TableColors {
     }
 }
 
-enum ShouldAddNewRow {
-    Yes,
-    No,
-}
-
 struct App {
     colors: TableColors,
     color_index: usize,
-    table_state: TableState,
     scroll_state: ScrollbarState,
-    character_index: usize,
-    transactions_table: TransactionsTable,
-    input: String,
-    error_msg: String,
+    input_page: InputPage,
+    show_help: bool,
 }
 
 impl App {
     fn new(file_path: PathBuf) -> Self {
+        let transactions_table = TransactionsTable::new(file_path);
+        let input_page = InputPage::new(transactions_table);
         Self {
             colors: TableColors::new(&PALETTES[0]),
             color_index: 0,
-            table_state: TableState::default().with_selected(0),
             scroll_state: ScrollbarState::new(0),
-            character_index: 1,
-            error_msg: "".to_string(),
-            transactions_table: TransactionsTable::new(file_path),
-            input: "".to_string(),
+            input_page,
+            show_help: false,
         }
-    }
-
-    fn update_editing_text(&mut self) {
-        if let Some((row, column)) = self.table_state.selected_cell() {
-            if let Some(editing_text) = self.transactions_table.get_cell_text(row, column) {
-                self.input = editing_text.clone();
-                self.error_msg = "".to_string();
-                self.character_index = self.input.chars().count();
-                self.transactions_table
-                    .update_recommended_input(row, column, &self.input);
-            }
-        }
-    }
-
-    fn update_selected(&mut self, i: usize) {
-        self.table_state.select(Some(i));
-        self.scroll_state = self.scroll_state.position(i * 4); // each row is of height 4
-        self.update_editing_text();
-    }
-
-    fn delete_transaction(&mut self) {
-        if let Some(i) = self.table_state.selected() {
-            self.transactions_table.delete_transaction(i);
-        }
-    }
-
-    fn next_row(&mut self, add_new_row_if_end: ShouldAddNewRow) {
-        let i = match self.table_state.selected() {
-            Some(i) => {
-                if i >= self.transactions_table.len() - 1 {
-                    match add_new_row_if_end {
-                        ShouldAddNewRow::Yes => {
-                            self.transactions_table.new_transaction();
-                            self.update_selected(self.transactions_table.len() - 1);
-                        }
-                        ShouldAddNewRow::No => {}
-                    }
-                    self.transactions_table.len() - 1
-                } else {
-                    i + 1
-                }
-            }
-            None => 0,
-        };
-        self.update_selected(i);
-    }
-
-    fn previous_row(&mut self) {
-        let i = match self.table_state.selected() {
-            Some(i) => {
-                if i == 0 {
-                    0
-                } else {
-                    i - 1
-                }
-            }
-            None => 0,
-        };
-        self.update_selected(i);
-    }
-
-    fn first_row(&mut self) {
-        let i = 0;
-        self.update_selected(i);
-    }
-
-    fn last_row(&mut self) {
-        let i = self.transactions_table.len() - 1;
-        self.update_selected(i);
-    }
-
-    fn select_first_column(&mut self) {
-        self.table_state.select_column(Some(0));
-    }
-
-    fn next_column(&mut self) {
-        if self.table_state.selected_column() == Some(TransactionField::widths().len() - 1) {
-            self.select_first_column();
-            self.next_row(ShouldAddNewRow::No);
-        } else {
-            self.table_state.select_next_column();
-        }
-        self.update_editing_text();
-    }
-
-    fn previous_column(&mut self) {
-        self.table_state.select_previous_column();
-        self.update_editing_text();
     }
 
     fn next_color(&mut self) {
@@ -199,134 +101,26 @@ impl App {
         self.colors = TableColors::new(&PALETTES[self.color_index]);
     }
 
-    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
-        new_cursor_pos.clamp(0, self.input.chars().count())
-    }
-
-    fn move_cursor_right(&mut self) {
-        let cursor_moved_right = self.character_index.saturating_add(1);
-        self.character_index = self.clamp_cursor(cursor_moved_right);
-    }
-
-    fn move_cursor_to_end(&mut self) {
-        let cursor_moved_to_end = self.input.len();
-        self.character_index = self.clamp_cursor(cursor_moved_to_end);
-    }
-
-    fn move_cursor_left(&mut self) {
-        let cursor_moved_left = self.character_index.saturating_sub(1);
-        self.character_index = self.clamp_cursor(cursor_moved_left);
-    }
-
-    fn move_cursor_home(&mut self) {
-        let cursor_moved_home = 0;
-        self.character_index = self.clamp_cursor(cursor_moved_home);
-    }
-
-    fn editing_text_byte_index(&self) -> usize {
-        self.input
-            .char_indices()
-            .map(|(i, _)| i)
-            .nth(self.character_index)
-            .unwrap_or(self.input.len())
-    }
-
-    fn update_recommendation(&mut self) {
-        if let Some((row, column)) = self.table_state.selected_cell() {
-            self.transactions_table
-                .update_recommended_input(row, column, &self.input);
-        }
-    }
-
-    fn enter_char(&mut self, ch: char) {
-        let index = self.editing_text_byte_index();
-        self.input.insert(index, ch);
-        self.move_cursor_right();
-        self.update_recommendation();
-    }
-
-    fn delete_char(&mut self) {
-        let is_not_cursor_leftmost = self.character_index != 0;
-        if is_not_cursor_leftmost {
-            let current_index = self.character_index;
-            let from_left_to_current_index = current_index - 1;
-
-            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
-            let after_char_to_delete = self.input.chars().skip(current_index);
-
-            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
-            self.move_cursor_left();
-            self.update_recommendation();
-        }
-    }
-
-    fn delete_char_forward(&mut self) {
-        let current_character_index = self.character_index;
-        self.move_cursor_right();
-        if self.character_index > current_character_index {
-            self.delete_char();
-        } else {
-            self.transactions_table.clear_recommended_input();
-        }
-    }
-
-    fn commit_input(&mut self) -> Result<(), String> {
-        if let Some((row, column)) = self.table_state.selected_cell() {
-            self.transactions_table
-                .update_transaction(row, column, &self.input)?;
-        }
-        Ok(())
-    }
-
-    fn handle_edit_events(&mut self, key: KeyEvent) -> Option<()> {
+    fn handle_key_events(&mut self, key: KeyEvent) -> Option<()> {
         if key.kind == KeyEventKind::Press {
             let ctrl_pressed = key.modifiers.contains(KeyModifiers::CONTROL);
             match key.code {
                 KeyCode::Esc => return Some(()),
-                KeyCode::Enter => match self.commit_input() {
-                    Ok(()) => {
-                        self.select_first_column();
-                        self.next_row(ShouldAddNewRow::Yes);
-                    }
-                    Err(error) => self.error_msg = error.to_string(),
-                },
-                KeyCode::Tab => match self.commit_input() {
-                    Ok(()) => self.next_column(),
-                    Err(error) => self.error_msg = error.to_string(),
-                },
-                KeyCode::BackTab => match self.commit_input() {
-                    Ok(()) => self.previous_column(),
-                    Err(error) => self.error_msg = error.to_string(),
-                },
-                KeyCode::Down => self.next_row(ShouldAddNewRow::No),
-                KeyCode::Up => self.previous_row(),
-                KeyCode::PageUp => self.first_row(),
-                KeyCode::PageDown => self.last_row(),
-
                 KeyCode::Char('c') if ctrl_pressed => self.next_color(),
-                KeyCode::Char('d') if ctrl_pressed => self.delete_transaction(),
-                KeyCode::Backspace => self.delete_char(),
-                KeyCode::Delete => self.delete_char_forward(),
-                KeyCode::Left => self.move_cursor_left(),
-                KeyCode::Right => self.move_cursor_right(),
-                KeyCode::End => self.move_cursor_to_end(),
-                KeyCode::Home => self.move_cursor_home(),
-                KeyCode::Char(char_to_insert) => self.enter_char(char_to_insert),
-                _ => {}
+                KeyCode::Char('h') if ctrl_pressed => self.show_help = true,
+                _ => self.input_page.handle_key_events(key),
             }
         }
         None
     }
 
     fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
-        self.transactions_table.load()?;
-        self.last_row();
-        self.next_column();
+        self.input_page.initialize_table()?;
         loop {
             terminal.draw(|frame| self.draw(frame))?;
             if let Event::Key(key) = event::read()? {
-                if let Some(_) = self.handle_edit_events(key) {
-                    self.transactions_table.save_transactions()?;
+                if let Some(_) = self.handle_key_events(key) {
+                    self.input_page.transactions_table.save_transactions()?;
                     return Ok(());
                 }
             }
@@ -334,62 +128,12 @@ impl App {
     }
 
     fn draw(&mut self, frame: &mut Frame) {
-        let vertical = &Layout::vertical([
-            Constraint::Length(4),
-            Constraint::Min(5),
-            Constraint::Length(3),
-        ]);
+        let vertical = &Layout::vertical([Constraint::Length(4), Constraint::Min(8)]);
         let rects = vertical.split(frame.area());
 
         self.render_instructions(frame, rects[0]);
-        self.render_table(frame, rects[1]);
+        self.input_page.draw(frame, rects[1], &self.colors);
         self.render_scrollbar(frame, rects[1]);
-        self.render_edit_bar(frame, rects[2]);
-        let cursor_y = rects[2].as_position().y + 1;
-        frame.set_cursor_position(Position::new(self.character_index as u16 + 1, cursor_y))
-    }
-
-    fn render_table(&mut self, frame: &mut Frame, area: Rect) {
-        let header_style = Style::default()
-            .fg(self.colors.header_fg)
-            .bg(self.colors.header_bg);
-        let selected_row_style = Style::default()
-            .add_modifier(Modifier::REVERSED)
-            .fg(self.colors.selected_row_style_fg);
-        let selected_col_style = Style::default().fg(self.colors.selected_column_style_fg);
-        let selected_cell_style = Style::default()
-            .add_modifier(Modifier::REVERSED)
-            .fg(self.colors.selected_cell_style_fg);
-
-        let header = TransactionField::names()
-            .into_iter()
-            .map(|name| Cell::from(name))
-            .collect::<Row>()
-            .style(header_style)
-            .height(1);
-        let rows = self
-            .transactions_table
-            .iter()
-            .enumerate()
-            .map(|(i, transaction)| {
-                let color = match i % 2 {
-                    0 => self.colors.normal_row_color,
-                    _ => self.colors.alt_row_color,
-                };
-                let row = transaction.generate_row();
-                row.style(Style::new().fg(self.colors.row_fg).bg(color))
-                    .height(3)
-            });
-        let bar = " █ ";
-        let t = Table::new(rows, TransactionField::widths())
-            .header(header)
-            .row_highlight_style(selected_row_style)
-            .column_highlight_style(selected_col_style)
-            .cell_highlight_style(selected_cell_style)
-            .highlight_symbol(Text::from(vec!["".into(), bar.into(), "".into()]))
-            .bg(self.colors.buffer_bg)
-            .highlight_spacing(HighlightSpacing::Always);
-        frame.render_stateful_widget(t, area, &mut self.table_state);
     }
 
     fn render_scrollbar(&mut self, frame: &mut Frame, area: Rect) {
@@ -420,26 +164,5 @@ impl App {
                     .border_style(Style::new().fg(self.colors.border_color)),
             );
         frame.render_widget(instructions, area);
-    }
-
-    fn render_edit_bar(&self, frame: &mut Frame, area: Rect) {
-        let edit_text = Line::from(vec![
-            Span::from(&self.input),
-            Span::from(&self.error_msg).fg(tailwind::ROSE.c600),
-            Span::from(self.transactions_table.get_recommended_input(&self.input))
-                .fg(tailwind::SLATE.c600),
-        ]);
-        let edit_bar = Paragraph::new(edit_text)
-            .style(
-                Style::new()
-                    .fg(self.colors.row_fg)
-                    .bg(self.colors.buffer_bg),
-            )
-            .block(
-                Block::bordered()
-                    .border_type(BorderType::Double)
-                    .border_style(Style::new().fg(self.colors.border_color)),
-            );
-        frame.render_widget(edit_bar, area);
     }
 }
